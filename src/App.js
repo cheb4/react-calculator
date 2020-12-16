59c070a003143755d35501d473a66e1b930efd8a
cheb4 <a.splitas@gmail.com>
2020-12-16 20:48:57 +0200

    working screen

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const App = () => {
     console.log(DisplayNumber);
   };
   //
-  const NumberButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const NumberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     .map((ele) => ele.toString(10))
     .map((num) => (
       <Btn
